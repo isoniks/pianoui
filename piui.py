771810272b24126f47197db5377e7a1ea580d1fb
isoniks <73814514+isoniks@users.noreply.github.com>
2020-11-04 02:27:52 +1100

    Update piui.py

--- piui.py
+++ piui.py
@@ -93,9 +93,9 @@ image = Image.new('RGB', (oled.WIDTH, oled.HEIGHT))  #for Pixelshift: (oled.WIDT
 oled.clear()
 
 font = load_font('Play-Bold.ttf', 26)                       #used for Artist
-font2 = load_font('Oxanium-Light.ttf', 12)                     #used for all menus
+font2 = load_font('Play-Regular.ttf', 12)                     #used for all menus
 font3 = load_font('Play-Regular.ttf', 22)                   #used for Song
-font4 = load_font('Oxanium-Medium.ttf', 14)                    #used for Format/Smplerate/Bitdepth
+font4 = load_font('Play-Regular.ttf', 14)                    #used for Format/Smplerate/Bitdepth
 hugefontaw = load_font('fa-solid-900.ttf', oled.HEIGHT - 4)    #used for play/pause/stop icons -> Status change overlay
 iconfont = load_font('entypo.ttf', oled.HEIGHT - 2)           #used for play/pause/stop/shuffle/repeat... icons
 fontClock = load_font('DSG.ttf', 41)                           #used for clock
